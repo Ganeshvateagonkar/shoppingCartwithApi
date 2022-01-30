64b0cc5e566a4dfe5b2357e41248bcca93276817
ganesh vategaonkar <ganeshvat17@gmail.com>
2022-01-30 02:18:09 -0800

    tic tack toe game completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import Icon from "./components/Icon";
+import { Card, Button, Row, Col, Container, CardBody } from "reactstrap";
 
+const itemArray = new Array(9).fill("empty");
 function App() {
+  const [isCross, setIsCross] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const reloadGame = () => {
+    setIsCross(false);
+    setMessage("");
+    itemArray.fill("empty", 0, 9);
+  };
+
+  const checkIsWinner = () => {
+    if (
+      itemArray[0] === itemArray[1] &&
+      itemArray[0] === itemArray[2] &&
+      itemArray[0] !== "empty"
+    ) {
+      setMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[3] !== "empty" &&
+      itemArray[3] === itemArray[4] &&
+      itemArray[4] === itemArray[5]
+    ) {
+      setMessage(`${itemArray[3]} won`);
+    } else if (
+      itemArray[6] !== "empty" &&
+      itemArray[6] === itemArray[7] &&
+      itemArray[7] === itemArray[8]
+    ) {
+      setMessage(`${itemArray[6]} won`);
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[3] &&
+      itemArray[3] === itemArray[6]
+    ) {
+      setMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[1] !== "empty" &&
+      itemArray[1] === itemArray[4] &&
+      itemArray[1] === itemArray[7]
+    ) {
+      setMessage(`${itemArray[1]} won`);
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] === itemArray[5] &&
+      itemArray[5] === itemArray[8]
+    ) {
+      setMessage(`${itemArray[2]} won`);
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[4] &&
+      itemArray[4] === itemArray[8]
+    ) {
+      setMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] === itemArray[4] &&
+      itemArray[4] === itemArray[6]
+    ) {
+      setMessage(`${itemArray[2]} won`);
+    }
+  };
+  const changeItem = (itemNumber) => {
+    if (message) {
+      return toast(message, { type: "success" });
+    }
+    if (itemArray[itemNumber] === "empty") {
+      itemArray[itemNumber] = isCross ? "cross" : "circle";
+      setIsCross(!isCross);
+    } else {
+      return toast("already filled", { type: "error" });
+    }
+    checkIsWinner();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-5">
+      <ToastContainer position="bottom-center" />
+      <Row>
+        <Col md={6} className="offset-md-3">
+          {message ? (
+            <div className="mt-2 mb-2">
+              <h1 className="text-success text-uppercase text-center">
+                {message}
+              </h1>
+              <Button color="success" block onClick={reloadGame}>
+                Reload game
+              </Button>
+            </div>
+          ) : (
+            <h1 className="text-center text-warning ">
+              {isCross ? "cross" : "circle "} turns
+            </h1>
+          )}
+          <div className="grid">
+            {itemArray.map((item, index) => (
+              <Card color="warning" onClick={() => changeItem(index)}>
+                <CardBody className="box">
+                  <Icon name={item} />
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
